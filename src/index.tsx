b26c5f4c598b81d9ad9dbcbcac2be81c7355dc9e
magiqfoxx <katarzyna.zemsta@vp.pl>
2021-03-22 11:52:22 +0100

    Added global style

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import App from "./components/App/App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import GlobalStyle from "./GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <GlobalStyle />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
